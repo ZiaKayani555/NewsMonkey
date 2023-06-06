afcb80bcdee4210e7d96b60c59ded1e0095f07f3
Zia <ziakayani.official@gmail.com>
2023-06-06 11:34:19 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h1>hello this is zia</h1>
+      </div>
+    )
+  }
+}
